e3f48e22bf3a2ca62b4d27b9ed1e44c64385f40d
mlagunas <mlagunas94@gmail.com>
2018-07-23 08:39:02 +0200

    Change final color normalization to be a numpy operation

--- render.py
+++ render.py
@@ -10,11 +10,11 @@ random.seed(123456)
 out_name = 'antialiasing_32'
 
 # canvas properties
-width = 200
-height = 100
+WIDTH = 200
+HEIGHT = 100
 
 # color parameters
-color_max = 255.99
+MAX_COLOR = 255.99
 
 # number of antialiasing samples
 antialiasing_samples = 32
@@ -34,28 +34,28 @@ world = HitableList(world)
 
 # start the rendering process and save result into ppm file 
 def main():
-    header = 'P3\n%d %d\n%d\n' % (width, height, 255)
+    header = 'P3\n%d %d\n%d\n' % (WIDTH, HEIGHT, 255)
 
     with open('images/' + out_name + '.ppm', 'w') as f:
         f.write(header)
 
         # Iterate over heigth and width
-        for j in tqdm(range(height - 1, 0, -1)):
-            for i in range(width):
+        for j in tqdm(range(HEIGHT - 1, 0, -1)):
+            for i in range(WIDTH):
 
                 # initialize color
                 color = Vec3(0., 0., 0.)
 
                 # antialiasing by sampling multiple times in the same pixel
                 for s in range(antialiasing_samples):
-                    u = (i + random.random()) / width
-                    v = (j + random.random()) / height
+                    u = (i + random.random()) / WIDTH
+                    v = (j + random.random()) / HEIGHT
 
                     ray = camera.get_ray(u, v)
                     color += get_color(ray, world)
 
                 # normalize color for the number of samples and the color range
-                color = color.apply(lambda x: x / antialiasing_samples * color_max)
+                color = color / antialiasing_samples * MAX_COLOR
                 pixel = '%d %d %d\n' % (color.x(), color.y(), color.z())
                 f.write(pixel)
 
